cdbe33ae31fcb3931d7ebedec6ea8720fe25d32c
Yonatan Sharoni <yonatan.sharoni@gmail.com>
2019-08-05 11:54:26 +0300

    added the Movie.js file

---
+++ src/components/Movie.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Movie extends Component {
+    constructor() {
+        super()
+        
+
+    }
+    render() {
+        return <div>Don`t Worry</div>
+    }
+}
+
+export default Movie
